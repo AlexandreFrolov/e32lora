fe7694508666c50e322bb84ee677928202b2c7c6
Александр Фролов <frolov@itmatrix.ru>
2020-08-28 10:21:07 +0300

    Add files

---
+++ E32LORA.ts
@@ -0,0 +1,86 @@
+
+/**
+ * E32LORA block
+ */
+//% weight=100 color=#00cc00 icon="\uf012" block="E32LORA"
+namespace E32LORA {
+
+//    const E32LORA_I2C_ADDR=0x68
+
+
+    function E32LORA_init() {
+    }
+
+    E32LORA_init()
+//    setStatus(0x08)
+
+
+    /**
+     * decToHexString
+     *
+     * https://stackoverflow.com/questions/50967455/from-decimal-to-hexadecimal-without-tostring
+     */
+    function decToHexString(int: number, base: number): string {
+        let letters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        let returnVal = "";
+        if (base > 1 && base < 37) {
+            while (int != 0) {
+                let rest = int % base;
+                int = Math.floor(int / base);
+                returnVal = letters[rest] + returnVal;
+            }
+        }
+        return returnVal;
+    }
+
+    function decToBcd(value: number): number {
+        return (Math.floor(value / 10) << 4) + (value % 10)
+    }
+
+    function bcdToDec(value: number): number {
+        return Math.floor(value / 16) * 10 + (value % 16)
+    }
+
+
+// ==========================================================================
+// Export Functions.
+// ==========================================================================
+
+
+
+
+
+
+// ==========================================================================
+// Advanced Export Functions
+// ==========================================================================
+
+
+    /**
+     * hexString
+     */
+    //% block
+    //% weight=20
+    export function hexString(value: number): string {
+        return decToHexString(value, 16)
+    }
+
+    /**
+     * binaryString
+     */
+    //% block
+    //% weight=19
+    export function binaryString(value: number): string {
+        return decToHexString(value, 2)
+    }
+
+    /**
+     * decimalString
+     */
+    //% block
+    //% weight=18
+    export function decimalString(value: number): string {
+        return decToHexString(value, 10)
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# E32LORA
+
+## MakeCode E32LORA RTC Extension for micro:bit
+
+
+![](e32lora.jpg)
+
+## Add extension
+
+Copy and paste this to MakeCode Extension search box:
+https://github.com/AlexandreFrolov/E32LORA
+
+
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
