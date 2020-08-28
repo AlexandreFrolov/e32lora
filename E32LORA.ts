b5f16b2919220aec0ee3a8538e30baeb786ab51a
Александр Фролов <frolov@itmatrix.ru>
2020-08-28 13:11:44 +0300

    Update E32LORA.ts

--- E32LORA.ts
+++ E32LORA.ts
@@ -15,35 +15,6 @@ namespace E32LORA {
 //    setStatus(0x08)
 
 
-    /**
-     * E32LORA class
-     */
-    export class E32LORA_WRL {
-        clk: DigitalPin;
-        dio: DigitalPin;
-        cs: DigitalPin;
-
-        /**
-         * create a E32LORA_WRL object.
-         * @param clk the CLK pin for E32LORA, eg: DigitalPin.P13
-         * @param dio the DIO pin for E32LORA, eg: DigitalPin.P14
-         * @param cs the CS pin for E32LORA, eg: DigitalPin.P15
-         */
-        //% weight=200 blockGap=8
-        //% blockId="E32LORA_create" block="CLK %clk|DIO %dio|CS %cs"
-        export create(clk: DigitalPin, dio: DigitalPin, cs: DigitalPin): E32LORA_WRL {
-            let ds = new E32LORA_WRL();
-            ds.clk = clk;
-            ds.dio = dio;
-            ds.cs = cs;
-            pins.digitalWritePin(ds.clk, 0);
-            pins.digitalWritePin(ds.cs, 0);
-            return ds;
-        }
-
-
-    }
-
 
 
       export function pins(clk: DigitalPin, dio: DigitalPin, cs: DigitalPin) {
