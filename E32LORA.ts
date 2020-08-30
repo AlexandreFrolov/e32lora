36c17c259f753e9a417120c440a356d940fcf22a
Александр Фролов <frolov@itmatrix.ru>
2020-08-30 18:56:17 +0300

    Update E32LORA.ts

--- E32LORA.ts
+++ E32LORA.ts
@@ -141,12 +141,11 @@ namespace E32LORA {
      * e32configNoSave
      */
     //% weight=46
-    //% block="E32LORA module config: | ADDR: %addr CHANNEL: %channel FIXED: %fixedm POWER: %pwr UART BAUD: %ubaud AIR BAUD: %airbaud"
+    //% block="E32LORA module config: | ADDR: %addr CHANNEL: %channel FIXED: %fixedm UART BAUD: %ubaud AIR BAUD: %airbaud POWER: %pwr"
     //% addr.defl="0000" channel.min=0 channel.max=31 channel.defl=15 fixedm.defl=false pwr.defl=0 pwr.min=0 pwr.max=3 ubaud.defl=UartBaud.BaudRate9600 airbaud.defl=AirBaud.BaudRate2400
     export function e32configNoSave(addr: string, channel: number, fixedm: boolean, pwr: number, ubaud: UartBaud, airbaud: AirBaud): string {
 
         let _uartbaud: NumberFormat.UInt8LE = parseInt(ubaud);
-//        let _airbaud: NumberFormat.UInt8LE = parseInt("2");
         let _airbaud: NumberFormat.UInt8LE = parseInt(airbaud);
         let byte3: NumberFormat.UInt8LE = ((_uartbaud << 3) + _airbaud) & 0x3f;
         let byte3String: string = decToHexString(byte3, 16);
