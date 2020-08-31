d3d16272de5670eb7430c705240e3e30f4f4f2f5
Александр Фролов <frolov@itmatrix.ru>
2020-08-31 09:58:22 +0300

    Update E32LORA.ts

--- E32LORA.ts
+++ E32LORA.ts
@@ -142,8 +142,10 @@ namespace E32LORA {
      */
     //% weight=46
     //% block="E32LORA module config: | ADDR: %addr CHANNEL: %channel FIXED: %fixedm UART BAUD: %ubaud AIR BAUD: %airbaud POWER: %pwr SAVE CONFIG: %save"
-    //% addr.defl="0000" channel.min=0 channel.max=31 channel.defl=15 fixedm.defl=false ubaud.defl=UartBaud.BaudRate9600 airbaud.defl=AirBaud.BaudRate2400 pwr.defl=0 pwr.min=0 pwr.max=3 save.defl=false
-    export function e32config(addr: string, channel: number, fixedm: boolean, ubaud: UartBaud, airbaud: AirBaud, pwr: number, save: boolean): string {
+    //% addr.defl=0 addr.min=0 addr.max=65535 channel.min=0 channel.max=31 channel.defl=15 fixedm.defl=false ubaud.defl=UartBaud.BaudRate9600 airbaud.defl=AirBaud.BaudRate2400 pwr.defl=0 pwr.min=0 pwr.max=3 save.defl=false
+    export function e32config(addr: NumberFormat.UInt16LE, channel: number, fixedm: boolean, ubaud: UartBaud, airbaud: AirBaud, pwr: number, save: boolean): string {
+
+        let addrString = decToHexString(addr, 16);
 
         let byte1: NumberFormat.UInt8LE = 0;
         if(save == true) {
@@ -171,7 +173,7 @@ namespace E32LORA {
         }
         let byte5String  = decToHexString(byte5, 16);
 
-        let cmdBuffer=Buffer.fromHex(byte1String + addr + "1a" + byte4String + byte5String)
+        let cmdBuffer=Buffer.fromHex(byte1String + addrString + "1a" + byte4String + byte5String)
 //        let cmdBuffer=Buffer.fromHex("c2" + addr + "1a" + byte4String + byte5String)
         return buffer2string(cmdBuffer);
     }
